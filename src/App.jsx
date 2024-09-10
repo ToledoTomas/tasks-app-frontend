56f0ae13cb659d8ee830bb164c341b9c0f82ace8
unknown <tomas_toledo98@hotmail.com>
2024-09-10 18:23:48 -0300

    adding react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./views/HomePage";
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline text-center mt-6">
-        Hello World!
-      </h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<h1>Login</h1>} />
+        <Route path="/register" element={<h1>Register</h1>} />
+        <Route path="/tasks" element={<h1>Tasks</h1>} />
+        <Route path="/add-tasks" element={<h1>New Task</h1>} />
+        <Route path="/task/:id" element={<h1>Update Task</h1>} />
+        <Route path="/profile" element={<h1>Profile</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
